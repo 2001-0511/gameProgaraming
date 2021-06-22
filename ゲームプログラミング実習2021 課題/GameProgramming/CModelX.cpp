50f3f0af256cd8d6e2036b892f5b847053e485e8
Chihiro0955 <chihirodegozaru0955@outlook.jp>
2021-06-22 14:36:19 +0900

    kadai

--- ゲームプログラミング実習2021 課題/GameProgramming/CModelX.cpp
+++ ゲームプログラミング実習2021 課題/GameProgramming/CModelX.cpp
@@ -190,6 +190,15 @@ void CMesh::Init(CModelX *model){
 			}
 			model->GetToken();      // } //END of MEshMaterialList
 		}
+		//SkinWeightsのとき
+		else if (strcmp(model->mToken, "SkinWeights") == 0){
+			//CSkinWeightsクラスのインスタンスを作成し,配列に追加
+			mSkinWeights.push_back(new CSkinWeights(model));
+		}
+		else{
+			//以外のノードは読み飛ばし
+			model->SkipNode();
+		}
 		
 
 		printf("VertexNum:%d\n", mVertexNum);
@@ -205,6 +214,7 @@ void CMesh::Init(CModelX *model){
 			printf(" %2d ", mpVertexIndex[i + 1]);
 			printf(" %2d\n", mpVertexIndex[i + 2]);
 		}
+		
 	}
 }
 
@@ -292,6 +302,53 @@ CModelXFrame::CModelXFrame(CModelX* model){
 
 #endif
 }
+
+/*
+CSkinWeights
+スキンウェイトの読み込み
+*/
+CSkinWeights::CSkinWeights(CModelX *model)
+:mpFrameName(0)
+, mFrameIndex(0)
+, mIndexNum(0)
+, mpIndex(nullptr)
+, mpWeight(nullptr)
+{
+	model->GetToken();   //{
+	model->GetToken();   //FrameName
+	//フレーム名エリア確保、設定
+	mpFrameName = new char[strlen(model->mToken) + 1];
+	strcpy(mpFrameName, model->mToken);
+	//頂点番号数取得
+	mIndexNum = model->GetIntToken();
+	//頂点番号数が0を超える
+	if (mIndexNum > 0){
+		//頂点番号と頂点ウェイトのエリア確保
+		mpIndex = new int[mIndexNum];
+		mpWeight = new float[mIndexNum];
+		//頂点番号取得
+		for (int i = 0; i < mIndexNum; i++)
+			mpIndex[i] = model->GetIntToken();
+		//頂点ウェイト取得
+		for (int i = 0; i < mIndexNum; i++)
+			mpWeight[i] = model->GetFloatToken();
+	}
+	//オフセット行列取得
+	for (int i = 0; i < 16; i++){
+		mOffset.mF[i] = model->GetFloatToken();
+	}
+	model->GetToken();     //}
+#ifdef _DEBUG
+	printf("SkinWeights:%s\n", mpFrameName);
+	for (int i = 0; i < mIndexNum; i++) {
+		printf("%d", mpIndex[i]);
+		printf("%10f\n", mpWeight[i]);
+	}
+	mOffset.Print();
+#endif
+
+}
+
 
 /*
 GetFloatToken
